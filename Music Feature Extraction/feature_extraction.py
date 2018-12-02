48c8b326041db1a3c961dabc917ce0670afe167a
LePython <nikarka1@gmail.com>
2018-12-02 15:05:02 +0100

    7 Features
    Former-commit-id: 840199c912e0c985a48da46e6201d53f6a86e9d3

--- Music Feature Extraction/feature_extraction.py
+++ Music Feature Extraction/feature_extraction.py
@@ -9,7 +9,7 @@ from tqdm import tqdm
 WAV_DIR = 'audio_files/'
 wav_files = os.listdir(WAV_DIR)
 
-col_names = ['file_name', 'spectral_contrast_2_mean', 'mfccs_4_mean', 'mfccs_1_std']
+col_names = ['file_name', 'tempo', 'spectral_contrast_1_mean', 'spectral_contrast_2_mean', 'spectral_contrast_3_mean', 'mfccs_4_mean', 'mfccs_1_std', 'mfccs_5_std']
             
             
 df = pd.DataFrame(columns=col_names)
@@ -21,12 +21,19 @@ for f in tqdm(wav_files):
         
         feature_list = [f]
 
+        tempo = librosa.beat.tempo(y, sr=sr)
+        feature_list.extend(tempo)
+
         spectral_contrast = librosa.feature.spectral_contrast(y, sr=sr, n_bands = 6, fmin = 200.0)
+        feature_list.append(np.mean(spectral_contrast[1]))
         feature_list.append(np.mean(spectral_contrast[2]))
+        feature_list.append(np.mean(spectral_contrast[3]))
 
         mfccs = librosa.feature.mfcc(y, sr=sr, n_mfcc=20)
         feature_list.append(np.mean(mfccs[4]))
         feature_list.append(np.std(mfccs[1]))
+        feature_list.append(np.std(mfccs[5]))
+
         
         feature_list[1:] = np.round(feature_list[1:], decimals=3)
         
