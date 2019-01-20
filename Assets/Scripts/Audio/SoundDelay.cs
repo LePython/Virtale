faccdd3a82df8be3ca3c20b39a794f7b15788873
LePython <nikarka1@gmail.com>
2019-01-20 16:46:23 +0100

    Spawn
    Former-commit-id: 3c3c16f4160c2a78d3f21e60f359fac2b1d1c48a

---
+++ Assets/Scripts/Audio/SoundDelay.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class SoundDelay : MonoBehaviour
+{
+    [SerializeField]
+    private UnityEvent OnPlayEnd;
+
+    [SerializeField]
+    private float delay;
+
+    private AudioSource audioSource;
+    private IEnumerator delayPlay()
+    {
+        yield return new WaitForSeconds(delay);
+        audioSource.Play();
+        
+        while((audioSource.clip.length + delay) > Time.time)
+        {
+            yield return null;
+        }
+        OnPlayEnd.Invoke();
+        
+        yield break;
+    }
+    private void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+        if(!audioSource)
+        {
+            enabled = false;
+        }
+        audioSource.playOnAwake = false;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(delayPlay());   
+    }
+}
---
+++ Assets/Scripts/VRRig/FadingScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class FadingScript : MonoBehaviour
+{
+    [SerializeField]
+    private UnityEvent OnFadeFinished;
+    private Animator animator;
+
+    private IEnumerator WaitForFadeStateChange(float delay)
+    {
+        float currentTime = Time.time;
+
+        while(Time.time - currentTime > delay)
+        {
+            yield return null;
+        }
+
+        OnFadeFinished.Invoke();
+
+        yield break;
+    }
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+    public void SetFadeState(bool stet)
+    {
+        animator.SetBool("FadeIn", stet);
+        StartCoroutine(WaitForFadeStateChange(1.0f));
+    }
+}
