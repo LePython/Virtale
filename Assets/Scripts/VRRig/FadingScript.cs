ab5c05fa629caa26d5c26ebf9d345307163431d9
LePython <nikarka1@gmail.com>
2019-01-22 18:34:50 +0100

    New Scene for logo
    Former-commit-id: 542491b04836570ae2bded9d6fd6f00afe4790e1 [formerly 8bd4398138904b6c38a073cab807f53ca6756897]
    Former-commit-id: a141566ec89e9156fcc5cb89bdb3a530614b4cfb

--- Assets/Scripts/VRRig/FadingScript.cs
+++ Assets/Scripts/VRRig/FadingScript.cs
@@ -2,24 +2,44 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.SceneManagement;
 
 public class FadingScript : MonoBehaviour
 {
     [SerializeField]
-    private UnityEvent OnFadeFinished;
+    private UnityEvent OnFadeInFinished;
     private Animator animator;
 
     private void Awake()
     {
         animator = GetComponent<Animator>();
     }
-    public void SetFadeState(bool stet)
+    private void Start()
     {
-        animator.SetBool("FadeIn", stet);
-        Invoke("InvokeOnFade", 1f);
+        SetFadeState(false);
     }
-    private void InvokeOnFade()
+    // If you want to fade in, set the bool to true. False otherwise.
+    public void SetFadeState(bool fadein)
     {
-        OnFadeFinished.Invoke();
+        animator.SetBool("FadeIn", fadein);
+        if(fadein == true)
+            Invoke("InvokeOnFadeInEnd", 1f);
+    }
+    private void InvokeOnFadeInEnd()
+    {
+        OnFadeInFinished.Invoke();
+    }
+    public void LoadScene(int sceneNumber)
+    {
+        StartCoroutine(LoadSceneAsync(sceneNumber));
+    }
+    private IEnumerator LoadSceneAsync(int sceneIndex)
+    {
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(sceneIndex);
+
+        while(!asyncLoad.isDone)
+        {
+            yield return null;
+        }
     }
 }
