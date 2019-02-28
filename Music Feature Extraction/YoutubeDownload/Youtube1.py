e01940a4c669ed803b6283cb4eca70956f300178
Noa Sakurajin <14131388+noah1510@users.noreply.github.com>
2019-02-28 16:43:13 +0100

    created Youtube1.py for external download starting
    Former-commit-id: 389d2c7b42a1e4314b1b7a9708ae5e8fa081ef3c [formerly 21e6d8f05e2cd56fe0b7bf6fb738fe77d0139a45]
    Former-commit-id: e10a8e66502dde29f9a0928e008710915dd9fb83

---
+++ Music Feature Extraction/YoutubeDownload/Youtube1.py
@@ -0,0 +1,89 @@
+from __future__ import unicode_literals
+from pydub import AudioSegment
+import youtube_dl
+import sys
+import os
+import json
+import time
+
+dir_path = os.path.dirname(os.path.realpath(__file__))
+
+def download_wav(song_url, format):
+
+    outtmpl = os.path.normpath(os.getcwd() + os.sep + os.pardir + "/audio_files/%(title)s.%(ext)s")
+    print(outtmpl)
+    ydl_opts = {
+        "format": "bestaudio/best",
+        "outtmpl": outtmpl,
+        "postprocessors": [{
+            "key": "FFmpegExtractAudio",
+            "preferredcodec": format
+        }],
+        "ignoreerrors":True
+    }
+
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        ydl.download([song_url])
+
+        info_dict = ydl.extract_info(song_url, download=False)
+        return os.path.normpath(os.getcwd() + os.sep + os.pardir + "/audio_files/" + info_dict.get('title', None) + "." + format)
+
+
+# opening the task file
+with open(os.path.normpath(dir_path + "/task.json")) as aFile:
+    task = json.load(aFile)
+    print(task)
+    if task["stage"] == 'download':
+        print("starting download")
+        try:
+            file = download_wav(task["url"],task["format"])   
+        except Exception as e:
+            print("The url is invalid. Please enter a valind url!", e)
+        else:  
+            next_file = { 
+                "stage" : "analysis"
+            }
+
+            try:
+                # openening task file to check if not overwriting something
+                print("checking for running analysis")
+                while next_file["stage"] != 'finished':
+                    time.sleep(1)
+                    f = open(os.path.normpath(dir_path + "/task1.json"), "r")
+                    next_file = json.load(f)
+
+            except Exception as e:
+                print("Something went wrong", e)
+
+            else:
+
+                try:
+                    # overwriting current taskfile
+                    print("updating current task")
+                    task["stage"] = 'finished'
+                    f = open(os.path.normpath(dir_path + "/task.json"), "w")
+                    f.write(json.dumps(task))
+                except Exception as e:
+                    print("Something went wrong", e)    
+
+                else:
+
+                    try:
+                        # updating properties of json file
+                        task["stage"] = 'analysis'
+                        task["location"] = str(file)
+
+                        # writing to new properties file for next step
+                        print("updating next task")
+                        f = open(os.path.normpath(dir_path + "/task1.json"), "w")
+                        f.write(json.dumps(task))
+
+                    except Exception as e:
+                        print("Something went wrong", e) 
+                    else:
+                        print("finished without errors")
+                        
+    else:
+        print("invalid type!")
+        
+
