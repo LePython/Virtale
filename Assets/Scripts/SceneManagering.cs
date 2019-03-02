49c73e3e848f9584118aaecf920bf3620d4e7ed1
LePython <nikarka1@gmail.com>
2019-03-02 16:02:18 +0100

    Scene transition with arrow keys for an ad (so that my pc doesn't shut down lol)
    Former-commit-id: df052e84986b26d7c17533ca4d20d29006122721 [formerly 6b32d010020039962bbfe0067189bc6ed1474a0f]
    Former-commit-id: b986871b2bf9142537221c53902cfce0e9402c7d

---
+++ Assets/Scripts/SceneManagering.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneManagering : MonoBehaviour
+{
+    [SerializeField]
+    private int scene;
+
+    public void ChangeScene(int sceneNumber)
+    {
+        SceneManager.LoadScene(sceneNumber);
+    }
+
+    private void Update() {
+        if(Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            ChangeScene(scene);
+        }
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            ChangeScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+    void OnEnable()
+    {
+        SceneManager.sceneLoaded += OnLevelFinishedLoading;
+    }
+    void OnDisable()
+    {
+        SceneManager.sceneLoaded -= OnLevelFinishedLoading;
+    }
+    private void OnLevelFinishedLoading(Scene scene, LoadSceneMode mode)
+    {
+        UnityEngine.AudioManager.AudioPlaybackManager.musicPlaybackState = UnityEngine.AudioManager.AudioPlaybackManager.PlaybackState.Pause;
+        UnityEngine.AudioManager.AudioPlaybackManager.SongNumber = 0;
+        Debug.Log("ss");
+    }
+}
