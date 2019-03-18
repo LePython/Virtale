125f6af89aa12c036697a528c56e3f0905809df2
LePython <nikarka1@gmail.com>
2019-03-18 20:35:00 +0100

    Disable main panel when hand is out of sensor field of view
    Former-commit-id: 95bd7b849690365f7d9c5c4c086c4f3ee5e159e6

---
+++ Assets/Scripts/VRRig/HandOutsideCameraView.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class HandOutsideCameraView : MonoBehaviour
+{
+    
+
+    [SerializeField]
+    private GameObject objectToObserveForChanges;
+
+    [SerializeField]
+    private UnityEvent WhenObjectDisabled;
+
+    public void StartObjectActiveStateObservation()
+    {
+        StartCoroutine("ObjectActiveStateObservation");
+    }
+    private IEnumerator ObjectActiveStateObservation()
+    {
+        if(objectToObserveForChanges.activeSelf == true)
+        {
+            yield return new WaitForSeconds(0.2f);
+        }
+
+        WhenObjectDisabled.Invoke();
+
+        yield break;
+    }
+}
---
+++ Assets/Scripts/VRRig/LeftHandOnAppear.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class LeftHandOnAppear : MonoBehaviour
+{
+    
+    [SerializeField]
+    private UnityEvent OnLeftHandAppear;
+
+    private void OnEnable() 
+    {
+        OnLeftHandAppear.Invoke();
+    }
+}
